46790100f48c70a62caceffc36fe871110554b95
marcelopaludetto <marcelo.paludetto@alura.com.br>
2023-06-11 15:15:39 -0300

    com gráfico

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ lerArquivoCSV(
 )
   .then((valores) => {
     const titulos = valores.splice(0, 1);
-
-    const nomes = valores.splice(1);
-    console.log(nomes[0].length);
+    const nomes = valores.splice(0);
+    console.log(nomes);
 
     var thead = document.querySelector(".tabela-titulo");
     for (var k = 0; k < titulos[0].length; k++) {
@@ -15,14 +14,35 @@ lerArquivoCSV(
     }
     for (var i = 0; i < nomes.length; i++) {
       var row = document.createElement("tr");
+
       for (var j = 0; j < nomes[0].length; j++) {
         var cell = document.createElement("td");
         cell.textContent = nomes[i][j];
+
+        if (j == 3){
+          cell.classList.add("nota-matematica");
+        }
+        if (j == 0){
+          cell.classList.add("nome");
+        }
+        if (j == 2){
+          cell.classList.add("nota-portugues");
+        }
+
         row.appendChild(cell);
       }
 
       tbody.appendChild(row);
     }
+  
+    let listaNome = Array.from(document.querySelectorAll('.nome')).map(n => n.innerText);
+    let listaNotas = Array.from(document.querySelectorAll('.nota-matematica')).map(n => parseFloat(n.innerText));
+    let listaNotas2 = Array.from(document.querySelectorAll('.nota-portugues')).map(n => parseFloat(n.innerText));
+
+    grafico(listaNome,listaNotas,listaNotas2); 
+
+
+
   })
 
   .catch((error) => {
@@ -37,13 +57,47 @@ async function lerArquivoCSV(arquivo) {
     const linhas = data.split("\n");
     const valores = linhas.map((linha) => linha.split(","));
     return valores;
+
   } catch (error) {
     console.error("Erro ao ler o arquivo:", error);
   }
 }
 
-
 var tagH2 = document.querySelector(".titulo-tabela");
-tagH2.innerHTML = 'Notas da Turma para o Vestibular';
+tagH2.innerHTML = 'Notas da Turma da 3ª série';
 var tbody = document.querySelector("#minha-tabela");
 
+
+function grafico(listaNome,listaNotas,listaNotas2) {
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let chart = new Chart(ctx, {
+      type: 'bar', // ou 'line' para um gráfico de linha
+      data: {
+          labels: listaNome,
+          datasets: [{
+              label: 'Notas de Matemática',
+              data: listaNotas,
+              backgroundColor: 'rgba(75, 192, 192, 0.2)',
+              borderColor: 'rgba(75, 192, 192, 1)',
+              borderWidth: 1
+          },
+          {
+            label: 'Notas de Português',
+            data: listaNotas2,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+}
